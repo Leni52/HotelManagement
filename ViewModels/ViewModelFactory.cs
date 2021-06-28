baefad3a37a79ca8af7c4b6715b5ac4ec6ef952b
Lenche <lencegeorgieva@yahoo.com>
2021-06-28 18:32:31 +0200

    adding

--- ViewModels/ViewModelFactory.cs
+++ ViewModels/ViewModelFactory.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace HotelManagement.Models
 {
+    /// <summary>
+    /// ROOMS
+    /// </summary>
     public class ViewModelFactory
     {
         public static RoomViewModel Details(Room r)
@@ -21,8 +24,6 @@ namespace HotelManagement.Models
                 RoomTypes = r == null ? Enumerable.Empty<RoomType>() :
                   new List<RoomType> { r.RoomType }
              
-
-
 
             };
         }
@@ -49,14 +50,12 @@ namespace HotelManagement.Models
         ///
 
         
-        public static RoomViewModel Edit(Room room, IEnumerable<RoomType> roomTypes, 
-            IEnumerable<Review> reviews)
+        public static RoomViewModel Edit(Room room, IEnumerable<RoomType> roomTypes)
         {
             return new RoomViewModel
             {
                 Room = room,
                 RoomTypes = roomTypes,
-                Reviews = reviews,
                 Theme = "warning",
                 Action = "Edit"
             };
@@ -66,15 +65,14 @@ namespace HotelManagement.Models
         //////DELETE
         ///
 
-        public static RoomViewModel Delete(Room room, IEnumerable<RoomType> roomTypes, 
-            IEnumerable<Review> reviews)
+        public static RoomViewModel Delete(Room room, IEnumerable<RoomType> roomTypes)
         {
             return new RoomViewModel
             {
 
                 Room = room,
                 RoomTypes = roomTypes,
-                Reviews = reviews,
+                
                 Theme = "danger",
                 Action = "Delete",
                 ReadOnly = true
